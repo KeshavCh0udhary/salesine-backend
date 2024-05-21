678df008c03dcfcc4fbf1364ba186d630f63ac4f
Krishna Keshav <Krishna2000chy@gmail.com>
2024-05-21 23:34:15 +0530

    Configure NestJS for Vercel deployment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,7 +17,7 @@ async function bootstrap() {
   };
   
   app.enableCors(corsOptions);
-
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
